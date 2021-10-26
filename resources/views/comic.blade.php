b36eb624aa55b3ac20d551da7045f736bb69186d
andmalt <andrea.maltempi@gmail.com>
2021-10-26 19:01:43 +0200

    adjusted comic.blade

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -9,6 +9,14 @@
         <img src="" alt="">
     </div>
 </div>
-
+<div id="top-p">
+    <div id="p-p">
+        <h3>Action Comic #1000</h3>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque accusantium blanditiis tempore pariatur ab suscipit vitae sunt beatae facere illum eos commodi vel velit nemo cum exercitationem, alias natus sapiente.</p>
+    </div>
+    <div id="p-img">
+        <img src="{{ asset('images/adva.jpg') }}" alt="image adv">
+    </div>
+</div>
 
 @endsection
